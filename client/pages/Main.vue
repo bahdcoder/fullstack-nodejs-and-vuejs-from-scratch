578923642eee6a2eeb78f2f70743a79297ba5fd6
Kati Frantz <bahdcoder@gmail.com>
2019-03-27 00:10:11 +0100

    lesson-14-design-register

--- client/pages/Main.vue
+++ client/pages/Main.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <div class="h-2 w-full bg-gold-light"></div>
-        <div class="w-full h-12 flex items-center justify-between px-6">
-            <div class="text-gold">Mevn</div>
+        <div class="py-5 mx-10 flex justify-between items-center">
+            <router-link to="/" class="no-underline text-gold">Mevn</router-link>
             <div>
                 <router-link class="no-underline text-brown" to="/auth/login">Sign In</router-link>
-                <router-link class="no-underline text-brown border-2 p-1 rounded-full border-brown ml-3"  to="/auth/register">Join Now</router-link>
+                <router-link class="no-underline text-brown border-2 px-3 py-2 hover:text-brown-darkest hover:border-brown-darkest rounded-full border-brown ml-3"  to="/auth/register">Join Now</router-link>
             </div>
         </div>
         <router-view></router-view>
--- client/pages/Register.vue
+++ client/pages/Register.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>sign up</h1>
+    <div class="container my-16 w-full mx-auto">
+        <div class="max-w-sm mx-auto">
+            <h2 class="text-center text-gold">Register</h2>
+
+            <div class="w-full bg-white shadow-md mt-5 rounded-sm p-12">
+                <input type="text" class="w-full text-xs focus:outline-none bg-brown-lightest p-5 text-brown mb-3" placeholder="Enter Your name">
+                <input type="text" class="w-full text-xs focus:outline-none bg-brown-lightest p-5 text-brown mb-3" placeholder="Enter Your email">
+                <input type="text" class="w-full text-xs focus:outline-none bg-brown-lightest p-5 text-brown" placeholder="Enter Your password">
+                <button class="w-full mt-3 text-sm py-5 bg-emerald text-white rounded-sm focus:outline-none hover:bg-emerald-light">
+                    Sign Up
+                </button>
+            </div>
+        </div>
+    </div>
 </template>
