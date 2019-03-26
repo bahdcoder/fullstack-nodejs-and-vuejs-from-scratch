b8eefd8399a7a34cef32f31bc72da1cdde4d19cd
Kati Frantz <bahdcoder@gmail.com>
2019-03-26 21:44:49 +0100

    lesson-7 add webpack dev middleware

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
-import Express from 'express'
-import Mongoose from 'mongoose'
+import path from 'path'
 import config from '@config'
+import Express from 'express'
+import Webpack from 'webpack'
 import v1Router from '@routes'
+import Mongoose from 'mongoose'
+import WebpackConfig from '@/webpack.config'
+import WebpackDevMiddleware from 'webpack-dev-middleware'
 
 Mongoose.connect(config.databaseUrl, { useNewUrlParser: true })
 
 const app = Express()
 
+const compiler = Webpack(WebpackConfig)
+
+app.use(WebpackDevMiddleware(compiler))
+
 app.use(v1Router)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 app.listen(3000, () => {
     console.log('server started succesfully.')
