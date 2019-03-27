2cfa854a560b93459cad4ebeea4c9437e5093860
Kati Frantz <bahdcoder@gmail.com>
2019-03-27 09:30:26 +0100

    lesson-19-hashing-user-passwords

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
+import Bcrypt from 'bcryptjs'
 import mongoose from 'mongoose'
+import randomstring from 'randomstring'
 
 const UserSchema = new mongoose.Schema({
     name: String,
@@ -8,6 +10,13 @@ const UserSchema = new mongoose.Schema({
     password: String,
     emailConfirmedAt: Date,
     emailConfirmCode: String
+})
+
+UserSchema.pre('save', function () {
+    this.password = Bcrypt.hashSync(this.password)
+    this.emailConfirmCode = randomstring.generate(72)
+
+    this.createdAt = new Date()
 })
 
 export default mongoose.model('User', UserSchema)
