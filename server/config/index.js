58527d954437fa816e70a6d658d577c05019ce2d
Kati Frantz <bahdcoder@gmail.com>
2019-03-27 09:43:52 +0100

    lesson-20-sending-account-confirmation-mails

--- server/config/index.js
+++ server/config/index.js
@@ -4,5 +4,6 @@ dotenv.config()
 
 export default {
     databaseUrl:
-        process.env.DATABASE_URL || 'mongodb://localhost:27017/mevnmongo'
+        process.env.DATABASE_URL || 'mongodb://localhost:27017/mevnmongo',
+    url: process.env.APP_URL || 'http://localhost:3000'
 }
--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,7 @@
+import config from '@config'
 import Bcrypt from 'bcryptjs'
 import mongoose from 'mongoose'
+import Mail from '@fullstackjs/mail'
 import randomstring from 'randomstring'
 
 const UserSchema = new mongoose.Schema({
@@ -17,6 +19,17 @@ UserSchema.pre('save', function () {
     this.emailConfirmCode = randomstring.generate(72)
 
     this.createdAt = new Date()
+})
+
+UserSchema.post('save', async function () {
+    await new Mail('confirm-account')
+        .to(this.email, this.name)
+        .subject('Please confirm your account')
+        .data({
+            name: this.name,
+            url: `${config.url}/auth/emails/confirm/${this.emailConfirmCode}`
+        })
+        .send()
 })
 
 export default mongoose.model('User', UserSchema)
